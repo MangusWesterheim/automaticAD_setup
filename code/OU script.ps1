e60d81d15c27f4e8532ca8a6e5431a50d331480a
saraleng <saraleng@stud.ntnu.no>
2022-03-15 15:53:41 +0100

    docs: Oppdatert OU skript

--- code/OU script.ps1
+++ code/OU script.ps1
@@ -1,56 +1,52 @@
 # User OUs
-#  Fix: Description and path.
 #  Add: More OUs?
 New-ADOrganizationalUnit 'AllUsers' -Description 'Containing OUs and users'
 New-ADOrganizationalUnit 'IT' -Description 'IT staff' `
-  -Path 'OU=AllUsers,DC=sec,DC=core'
+  -Path 'OU=AllUsers,DC=corp,DC=contoso,DC=com'
 New-ADOrganizationalUnit 'Adm' -Description 'Administration' `
-  -Path 'OU=AllUsers,DC=sec,DC=core'
+  -Path 'OU=AllUsers,DC=corp,DC=contoso,DC=com'
 New-ADOrganizationalUnit 'Employee' -Description 'Regular employees' `
-  -Path 'OU=AllUsers,DC=sec,DC=core'
+  -Path 'OU=AllUsers,DC=corp,DC=contoso,DC=com'
 New-ADOrganizationalUnit 'Management' -Description 'Management' `
-  -Path 'OU=AllUsers,DC=sec,DC=core'
+  -Path 'OU=AllUsers,DC=corp,DC=contoso,DC=com'
 New-ADOrganizationalUnit 'Inactive' -Description 'Inactive users' `
-  -Path 'OU=AllUsers,DC=sec,DC=core'
+  -Path 'OU=AllUsers,DC=corp,DC=contoso,DC=com'
 New-ADOrganizationalUnit 'Finance' -Description 'Financial dep.'`
-  -Path 'OU=Adm,OU=AllUsers,DC=sec,DC=core'
+  -Path 'OU=Adm,OU=AllUsers,DC=corp,DC=contoso,DC=com'
 New-ADOrganizationalUnit 'Logistic' -Description 'Logistics dep.'`
-  -Path 'OU=Adm,OU=AllUsers,DC=sec,DC=core'
-New-ADOrganizationalUnit 'Marketing' -Description ‘Marketing dep.'`
-  -Path 'OU=Adm,OU=AllUsers,DC=sec,DC=core'
+  -Path 'OU=Adm,OU=AllUsers,DC=corp,DC=contoso,DC=com'
+New-ADOrganizationalUnit 'Marketing' -Description 'Marketing dep.'`
+  -Path 'OU=Adm,OU=AllUsers,DC=corp,DC=contoso,DC=com'
 
 
 
 
 # Computer OUs
-#  Fix: Description and path. 
-#  Check: Apostrophe after lines.
 #  Add: More OUs?
-New-ADOrganizationalUnit 'Computers' -Description 'Containing OUs and users laptops' `
-New-ADOrganizationalUnit 'Clients' -Description 'Containing OUs and users laptops' `
-  -Path 'OU=Computers,DC=sec,DC=core'
-New-ADOrganizationalUnit 'Servers' -Description 'Containing OUs and servers' `
-  -Path 'OU=Computers,DC=sec,DC=core'
+New-ADOrganizationalUnit 'Computers' -Description 'Containing OUs' `
+New-ADOrganizationalUnit 'Clients' -Description 'Containing OUs, user laptops, workstations' `
+  -Path 'OU=Computers,DC=corp,DC=contoso,DC=com'
+New-ADOrganizationalUnit 'Servers' -Description 'SRV1' `
+  -Path 'OU=Computers,DC=corp,DC=contoso,DC=com'
 New-ADOrganizationalUnit 'Employee' -Description 'Employee laptops' `
-  -Path 'OU=Clients,OU=Computers,DC=sec,DC=core'
+  -Path 'OU=Clients,OU=Computers,DC=corp,DC=contoso,DC=com'
 New-ADOrganizationalUnit 'Kiosk' -Description 'Front desk and cash register' `
-  -Path 'OU=Clients,OU=Computers,DC=sec,DC=core'
+  -Path 'OU=Clients,OU=Computers,DC=corp,DC=contoso,DC=com'
 
 
 
 # Group OUs
-#  Fix: Description and path.
-#  Add: More OUs?
+#  Fix: More OUs/less OUs depending on groups + descriptions
 New-ADOrganizationalUnit 'Groups' -Description 'Containing OUs' `
 New-ADOrganizationalUnit 'IT' -Description '...' `
-  -Path 'OU=Groups,DC=sec,DC=core'
-New-ADOrganizationalUnit 'Finance' -Description 'Financials…' `
-  -Path 'OU=Groups,DC=sec,DC=core'
+  -Path 'OU=Groups,DC=corp,DC=contoso,DC=com'
+New-ADOrganizationalUnit 'Finance' -Description 'Financials' `
+  -Path 'OU=Groups,DC=corp,DC=contoso,DC=com'
 New-ADOrganizationalUnit 'Users' -Description 'Active users' `
-  -Path 'OU=Groups,DC=sec,DC=core'
+  -Path 'OU=Groups,DC=corp,DC=contoso,DC=com'
 New-ADOrganizationalUnit 'Mail' -Description 'Mail distribution lists' `
-  -Path 'OU=Groups,DC=sec,DC=core'
+  -Path 'OU=Groups,DC=corp,DC=contoso,DC=com'
 New-ADOrganizationalUnit 'Security' -Description 'Containing OUs...' `
-  -Path 'OU=Groups,DC=sec,DC=core'
+  -Path 'OU=Groups,DC=corp,DC=contoso,DC=com'
 New-ADOrganizationalUnit 'Telephone' -Description 'Telephone system' `
-  -Path 'OU=Groups,DC=sec,DC=core'
+  -Path 'OU=Security,OU=Groups,DC=corp,DC=contoso,DC=com'
