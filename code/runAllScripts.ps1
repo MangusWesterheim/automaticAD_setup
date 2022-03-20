0e8989059db3df50eb6573d3c3f64ca2bae1df09
saraleng <saraleng@stud.ntnu.no>
2022-03-20 20:33:46 +0100

    fix: Sjekker om alle skript finnes før den kjører + write-output

--- code/runAllScripts.ps1
+++ code/runAllScripts.ps1
@@ -1,16 +1,30 @@
 ﻿######################################################################################
 # Dette skriptet kjører alle skriptene som setter opp AD-strukturen.                 #
 # Skriptene kjøres i riktig rekkefølge og stopper om et skript ikke blir funnet.     #
+# Alle skriptene må ligge i en mappe "Scripts" med path som anvist i Set-Location.   #
 #                                                                                    #
 ######################################################################################
+
+$text = @"
+   _____
+  / ____|             | |
+ | |      ___   _ __  | |_  ___   ___   ___
+ | |     / _ \ | '_ \ | __|/ _ \ / __| / _ \
+ | |____| (_) || | | || |_| (_) |\__ \| (_) |
+  \_____|\___/ |_| |_| \__|\___/ |___/ \___/
+
+"@
+
+$text
 
 Set-Location 'C:\Users\Administrator\Downloads\Scripts\'
 
 $scriptsList =
 @(
     .\ou.ps1                      # Oppretter OUer
-    .\redirect_comp.ps1           # Omdirigere/flytte maskiner fra container til OU
+    .\redirect_comp.ps1           # Omdirigerer/flytter maskiner fra container til OU
     .\AddUsers.ps1                # Oppretter brukere (kjører userCSVScripts.ps1)
+    .\createGroups.ps1            # Oppretter grupper.
     .\shares.ps1                  # Oppretter og deler shares-mapper
     .\LAPS.ps1                    # Installerer LAPS
     .\importGPO.ps1               # Importerer GPOer
@@ -21,20 +35,62 @@ $scriptsList =
 
 $i = 0
 
-# Kjører ett og ett skript, hvis det finnes:
+# Sjekker at den finner alle skriptene:
 foreach($script in $scriptsList)
 {
     if ($script) {
-        $script
         $i++
-        Write-Output "Kjører $script."
     } else {
-        Write-Output "Fant ikke $script"
         break
     }
 }
 
-# Alle skriptene har blitt kjørt:
-if ($i -eq 9) {
-    Write-Output "Konto klar."
-}
+Write-Output "******************************************************************************************"
+Write-Output "*                                      Oppretter OUer                                    *"
+Write-Output "******************************************************************************************"
+.\ou.ps1
+
+Write-Output "******************************************************************************************"
+Write-Output "*                    Omdirigerer/flytter maskiner fra container til OU                   *"
+Write-Output "******************************************************************************************"
+.\redirect_comp.ps1
+
+Write-Output "******************************************************************************************"
+Write-Output "*                       Oppretter brukere (kjører userCSVScripts.ps1)                    *"
+Write-Output "******************************************************************************************"
+.\AddUsers.ps1
+
+Write-Output "******************************************************************************************"
+Write-Output "*                                   Oppretter grupper.                                   *"
+Write-Output "******************************************************************************************"
+.\createGroups.ps1
+
+Write-Output "******************************************************************************************"
+Write-Output "*                             Oppretter og deler shares-mapper                           *"
+Write-Output "******************************************************************************************"
+.\shares.ps1
+
+Write-Output "******************************************************************************************"
+Write-Output "*                                      Installerer LAPS                                  *"
+Write-Output "******************************************************************************************"
+.\LAPS.ps1
+
+Write-Output "******************************************************************************************"
+Write-Output "*                                      Importerer GPOer                                  *"
+Write-Output "******************************************************************************************"
+.\importGPO.ps1
+
+Write-Output "******************************************************************************************"
+Write-Output "*                  Delegerer rettigheter for tilbakestilling av passord                  *"
+Write-Output "******************************************************************************************"
+.\delegateResetPassword.ps1
+
+Write-Output "******************************************************************************************"
+Write-Output "*                         Installerer restic og setter opp backup                        *"
+Write-Output "******************************************************************************************"
+.\resticSetup.ps1
+
+Write-Output "******************************************************************************************"
+Write-Output "*                          Installerer IIS og setter opp webside                         *"
+Write-Output "******************************************************************************************"
+.\IIS.ps1
