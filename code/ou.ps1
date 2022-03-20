1e5ef9ef15623d3450b7cc95530de4113dd6d833
Sara Lehne Engesvik <saraleng@stud.ntnu.no>
2022-03-20 12:47:19 +0100

    Oppdaterte kommentering ou.ps1

--- code/ou.ps1
+++ code/ou.ps1
@@ -1,4 +1,4 @@
-﻿#############################################################################################################
+#############################################################################################################
 # Dette skriptet importerer OUer fra en CSV-fil og oppretter de deretter til i AD.                          #
 # CSV-filen må inneholde parameterne name, path, og description.                                            #
 # Inspirert av skript fra:                                                                                  #
@@ -13,11 +13,11 @@ Import-Module activedirectory
 # Henter CSV-fil:
 Invoke-WebRequest -URI https://raw.githubusercontent.com/dh-ctrl/DCSG1005_Infra_ActiveDirectory/main/CSV/ou.csv -OutFile ou.csv
 
-Sjekker om filen finnes:
+# Sjekker om filen finnes:
 if ((Get-Item ou.csv).Exists) {
 
     # Leser og oppretter OUer fra CSV-fil (header: name;path;description):
-    $ADOU = Import-csv C:\Users\Administrator\Documents\ou.csv -Delimiter ";"
+    $ADOU = Import-csv ou.csv -Delimiter ";"
     foreach ($ou in $ADOU) {
         New-ADOrganizationalUnit `
         -Name $ou.name `
